76c6aae022433ea4dac3579f94b59e16316d42fb
Zbigniew Siciarz <zbigniew@siciarz.net>
2024-12-13 15:49:13 +0100

    Don't test on Python < 3.9

--- setup.py
+++ setup.py
@@ -25,12 +25,9 @@ setup(
         "click>=7.0",
         "dnspython<2 ; python_version<'3.10'",
         "eventlet>=0.20.1",
-        "eventlet>=0.21.0 ; python_version>='3.6'",
-        "eventlet>=0.25.0 ; python_version>='3.7'",
         "eventlet>=0.33.0 ; python_version>='3.10'",
         "kombu>=4.2.0",
         "kombu>=5.2.0 ; python_version>='3.10'",
-        "importlib-metadata<5 ; python_version<='3.7'",  # https://github.com/celery/celery/issues/7783
         "mock>=1.2",
         "path.py>=6.2",
         "pyyaml>=5.1",
@@ -61,38 +58,32 @@ setup(
             "docutils<0.18",  # https://github.com/sphinx-doc/sphinx/issues/9788
             "jinja2<3.1.0",  # https://github.com/readthedocs/readthedocs.org/issues/9037
         ],
-        'examples': [
+        "examples": [
             "nameko-sqlalchemy==0.0.1",
             "PyJWT==2.6.0",
             "moto==4.1.2",
             "bcrypt==3.1.3",
-            "regex==2018.2.21"
+            "regex==2018.2.21",
         ],
     },
     entry_points={
-        'console_scripts': [
-            'nameko=nameko.cli:cli',
+        "console_scripts": [
+            "nameko=nameko.cli:cli",
         ],
-        'pytest11': [
-            'pytest_nameko=nameko.testing.pytest'
-        ]
+        "pytest11": ["pytest_nameko=nameko.testing.pytest"],
     },
     zip_safe=True,
-    license='Apache License, Version 2.0',
+    license="Apache License, Version 2.0",
     classifiers=[
         "Programming Language :: Python",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Intended Audience :: Developers",
-    ]
+    ],
 )
